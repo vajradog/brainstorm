91b1e86d149375fad79d1cb3c95dc85eca0b4044
vajradog <vajradog@gmail.com>
2014-01-14 12:33:32 -0500

    changed routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user, notice: 'User Saved'
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :name, :username, :password, :password_confirmation, :bio)
+	end
 end
